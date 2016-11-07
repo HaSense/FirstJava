ddcbca7e5c3412033bfa206965d35cdf6025494a
hadori7 <shha.work@gmail.com>
2016-11-07 12:29:18 +0900

    commit new eclipse

--- FirstJava/src/com/usnschool/FirstJava.java
+++ FirstJava/src/com/usnschool/FirstJava.java
@@ -5,6 +5,7 @@ public class FirstJava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Java~!");
+		System.out.println("shha.work");
 	}
 
 }
